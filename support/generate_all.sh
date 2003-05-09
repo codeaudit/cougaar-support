7382ef4bd9f0961df9fe8c65d32cdf774f4ad4db
tom <tom>
2003-05-09 14:59:41 +0000

    New script to generate all webalizer files

---
+++ support/generate_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run webalizer on the cougaarforge.cougaar.org first
+/usr/bin/webalizer -c /etc/webalizer/cougaarforge-webalizer.conf
+
+for project in `ls /var/www/gforge-3.0/ | grep -v prototype | grep -v www | grep -v common`
+do
+	# create the usage directory if it's not there
+	if [ ! -e "/var/www/gforge-3.0/$project/usage/" ]; then
+		mkdir "/var/www/gforge-3.0/$project/usage/"
+	fi
+
+	# run webalizer on this project
+	/usr/bin/webalizer -c /etc/webalizer/$project-webalizer.conf
+done 
