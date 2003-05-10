25808fc7065469b17c0c839bd1297a5485c29def
tom <tom>
2003-05-10 02:26:01 +0000

    Now it copies over msfree.png

--- support/generate_all.sh
+++ support/generate_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# run webalizer on the cougaarforge.cougaar.org first
+# run webalizer on cougaarforge.cougaar.org first
 /usr/bin/webalizer -c /etc/webalizer/cougaarforge-webalizer.conf
 
 for project in `ls /var/www/gforge-3.0/ | grep -v prototype | grep -v www | grep -v common`
@@ -8,6 +8,7 @@ do
 	# create the usage directory if it's not there
 	if [ ! -e "/var/www/gforge-3.0/$project/usage/" ]; then
 		mkdir "/var/www/gforge-3.0/$project/usage/"
+		cp /var/www/gforge-3.0/www/usage/msfree.png /var/www/gforge-3.0/$project/usage/
 	fi
 
 	# run webalizer on this project
