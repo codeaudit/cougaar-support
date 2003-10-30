dd0633969eb74ab2ba9bb5e32e8209b986216e42
tom <tom>
2003-10-30 03:17:29 +0000

    test 4

--- support/update_errata.rb
+++ support/update_errata.rb
@@ -1,8 +1,5 @@
 #!/usr/local/bin/ruby
 
-
-# foo
-
 Dir.chdir("/home/gforge/cronjobs/")
 `cvs -d:pserver:anonymous@cougaar.org:/cvsroot/cougaar co errata`
 `cp errata/cougaar1046/errata.html /var/www/gforge-3.0/www/`
