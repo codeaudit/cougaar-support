0d35630f052db2fb4ef4e7dc07edb13b19dbd8b8
tom <tom>
2003-11-06 14:28:54 +0000

    *** empty log message ***

--- support/www/gather.rb
+++ support/www/gather.rb
@@ -1,11 +1,11 @@
 #!/usr/local/bin/ruby
 
-0.upto(23) {|x|
+16.upto(23) {|x|
 	puts "hour = #{x}"
 	if x < 10
-		print `grep "04/Nov/2003:0#{x}" /usr/local/var/httpd/log/access_log | wc -l`
+		print `grep "05/Nov/2003:0#{x}" /usr/local/var/httpd/log/access_log | wc -l`
 	else
-		print `grep "04/Nov/2003:#{x}" /usr/local/var/httpd/log/access_log | wc -l`
+		print `grep "05/Nov/2003:#{x}" /usr/local/var/httpd/log/access_log | wc -l`
 	end
 	puts ""
 }
