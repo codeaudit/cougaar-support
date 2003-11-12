320fe1517b22d6b6aada9ebc2aab0329870fe9b6
tom <tom>
2003-11-12 22:19:13 +0000

    *** empty log message ***

--- cf-dashboard/dashboard.rb
+++ cf-dashboard/dashboard.rb
@@ -21,12 +21,12 @@ end
 
 class Build 
 	CVS_ROOT = ":pserver:anonymous@cougaar.org:/cvsroot/"
+	TMP = "tmp/"
 	def initialize(reports_dir)
 		@projects = []
 		@reports_dir = reports_dir
-		@working_dir="tmp_" + Time.now.usec.to_s + "/"
-   	Dir.mkdir(@working_dir)
-   	Dir.mkdir(@working_dir + "build")
+   	Dir.mkdir(TMP) unless File.exist? TMP
+   	Dir.mkdir(TMP + "build")
    	Dir.mkdir(@reports_dir) unless File.exist?(@reports_dir)
 	end
 	def add_project(p)
@@ -37,18 +37,18 @@ class Build
 	def build
 		get_third_party_jars()
 		@projects.each {|p|
-			cmd = "ant -v -listener org.apache.tools.ant.XmlLogger -DXmlLogger.file=#{@reports_dir}#{p.ant_xml_output} -buildfile build.xml -logfile #{@reports_dir}#{p.ant_text_output} -Dcore.workdir=#{@working_dir} -Dcore.repository=#{p.repo} -Dcore.module=#{p.mod} -Dcore.cvsTag=#{p.tag} -Dcore.cvsroot=#{CVS_ROOT} checkout"
+			cmd = "ant -v -listener org.apache.tools.ant.XmlLogger -DXmlLogger.file=#{@reports_dir}#{p.ant_xml_output} -buildfile build.xml -logfile #{@reports_dir}#{p.ant_text_output} -Dcore.workdir=#{TMP} -Dcore.repository=#{p.repo} -Dcore.module=#{p.mod} -Dcore.cvsTag=#{p.tag} -Dcore.cvsroot=#{CVS_ROOT} -Dcore.srcdir=#{p.srcdir} checkout compile "
 			puts cmd
 			puts `#{cmd}`
 		}
 	end
 	def clean		
-		`rm -rf #{working_dir}`
+		`rm -rf TMP`
 	end
 end
 
 if __FILE__ == $0
 	b = Build.new("reports/")
-	b.add_project Project.new("core","core","src","B10_4")
+	b.add_project Project.new("core","javaiopatch","src","B10_4")
 	b.build
 end
