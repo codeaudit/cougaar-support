acd6d57b6e2b5face94de197b28ce1f076806608
tom <tom>
2004-06-22 14:46:27 +0000

    Adding spammer

---
+++ support/spam/mailer.rb
@@ -0,0 +1,18 @@
+#!/usr/local/bin/ruby
+
+names = []
+File.open("names.txt").each_line {|line|
+	line.split(",").each {|n| names << n.chomp}
+}
+
+#toaddr = "tom@infoether.com"
+toaddr = "opencougaar04@cougaar.org"
+names.each {|n|
+	begin
+		cmd = "mail #{toaddr} -s \"CFP: OpenCougaar Conference (colocated with AAMAS 2004)\" -b #{n} < message.txt"
+		puts cmd
+		`#{cmd}`
+	rescue Exception
+		puts "Failed to send to #{n}"
+	end
+}
