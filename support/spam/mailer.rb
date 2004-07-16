6df26e07676422586c19c7d9e76f196285275699
tom <tom>
2004-07-16 13:13:27 +0000

    Another message

--- support/spam/mailer.rb
+++ support/spam/mailer.rb
@@ -1,15 +1,14 @@
 #!/usr/local/bin/ruby
 
 names = []
-File.open("names.txt").each_line {|line|
-	line.split(",").each {|n| names << n.chomp}
-}
+#File.open("names.txt").each_line {|line| line.split(",").each {|n| names << n.strip.chomp} }
+names << "tom@infoether.com"
 
 #toaddr = "tom@infoether.com"
 toaddr = "noreply@cougaar.org"
 names.each {|n|
 	begin
-		cmd = "mail #{toaddr} -s \"CFP: OpenCougaar Conference (colocated with AAMAS 2004)\" -b #{n} < message.txt"
+		cmd = "mail #{toaddr} -s \"OpenCougaar Conference (colocated with AAMAS 2004): Program and Keynote\" -b #{n} < message.txt"
 		puts cmd
 		`#{cmd}`
 	rescue Exception
