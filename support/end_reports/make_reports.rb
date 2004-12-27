24862cdc1079afb07cff9c802dd48b537becafc7
tom <tom>
2004-12-27 20:58:50 +0000

    Various changes on the big day itself

--- support/end_reports/make_reports.rb
+++ support/end_reports/make_reports.rb
@@ -2,36 +2,44 @@
 
 def tarwikis
 	`mkdir cougaar_wikis`
+	puts "Globbing"
 	Dir.glob("/var/www/gforge-projects/**/wiki/").each {|d|
-		`tar -zcf #{d.split("/")[4]}.tar.gz #{d}`
+		puts "Processing #{d}"
+		`tar -zcf cougaar_wikis/#{d.split("/")[4]}.tar.gz #{d}`
 	}
-	`tar -zcf cougaar_wikis.tar.gz cougaar_wikis/`
-	`rm -rf cougaar_wikis/`
+	`tar -zcf wikis.tar.gz cougaar_wikis/`
+	#`rm -rf cougaar_wikis/`
 end
 
 def getdocs
-	`mkdir cougaar_docs`
-	# query DB for all docs and the project name
-	# for each project, create a dir
-	# for each doc, wget the doc and put it in the dir
-	# tar up project dir
-	# delete project dir
-	`tar -zcf cougaar_docs.tar.gz cougaar_docs/`
-	`rm -rf cougaar_docs/`
+	`psql -q -t -U gforge gforge -c "select g.unix_group_name, d.group_id, d.docid, d.filename from groups g, doc_data d where d.stateid = 1 and d.group_id = g.group_id and g.status = 'A' order by g.group_id" > files.txt`
+	Dir.mkdir("docs") if !File.exists?("docs")
+	File.read("files.txt").split("\n").each {|line|
+		proj, gid, did, name = *(line.split("|"))
+		proj.strip!
+		gid.strip!
+		did.strip!
+		name.strip!
+		puts "Processing #{name}"
+		url = "http://cougaar.org/docman/view.php/#{gid}/#{did}/#{name}"
+		cmd = "wget -q \"#{url}\""
+		`#{cmd}`
+		Dir.mkdir("docs/#{proj}") if !File.exists?("docs/#{proj}")
+		`mv "#{name}" docs/#{proj}/`
+	}
 end
 
 def tarbugs
-	`mkdir -p cougaar_bugs/bugzilla_db/`
-	`mkdir cougaar_bugs/bugzilla_src/`
-	`cp /var/backups/mysql/bugs/Tue.tar.gz cougaar_bugs/bugzilla_db/`	 
-	`cp -r /var/www/bugzilla/ cougaar_bugs/bugzilla_src/`
-	`tar -zcf cougaar_bugs.tar.gz cougaar_bugs/`
-	`rm -rf cougaar_bugs/`
+	`cp /var/backups/mysql/bugs/Mon.tar.gz mysql_bugzilla_database_backup.tar.gz`	 
+	`tar -zcf bugzilla-2.16.5-source.tar.gz /var/www/bugzilla-2.16.5/`
 end
 
 if ARGV[0] == "tarwikis"
+	tarwikis
 elsif ARGV[0] == "getdocs"
+	getdocs
 elsif ARGV[0] == "tarbugs"
+	tarbugs
 else
 	puts "Usage: make_reports.rb <tarwikis|getdocs|tarbugs>"
 end
